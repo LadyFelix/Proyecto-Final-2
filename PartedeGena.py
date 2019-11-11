b8499bc19d36b229e2b81e342de9e65c5a987ce7
LadyFelix <ladynicolefelixflores_0204@hotmail.com>
2019-11-11 09:37:10 -0500

    PArte de Mamposteria

--- PartedeGena.py
+++ PartedeGena.py
@@ -1,7 +1,7 @@
 import os
 os.system("cls")
 
-a= input("¿Que vas a calcular?(Capa Aisladora/Mamposteria/Piso/Techo)")
+a= input("¿Que vas a calcular?(Capa Aisladora/Mamposteria/Piso/Techo):")
 if a == "Capa Aisladora":
     b = input("Ingrese el tipo de capa aisladora: (Horizontal/Vertical)")
     c = float(input("Ingrese el ancho del muro en metros:"))
@@ -11,61 +11,39 @@ if a == "Capa Aisladora":
     print("Cemento:",x,"Kg")
 
 elif a == "Mamposteria":
-    b = int(input("Ingrese el ancho de la pared:"))
-    c = int(input("Ingrese el alto de la pared:"))
+    b = float(input("Ingrese el ancho de la pared:"))
+    c = float(input("Ingrese el alto de la pared:"))
     d = input("Ingrese el tipo de ladrillo:(King Kong/Pandereta)")
     if d == "King Kong":
-        e = input("El tipo de aparejo sera:(Soga/Cabeza)")
-        if e == "Soga":
-            cl = (b*c)/((0.24 + 0.015)*(0.09+ 0.015))
-            cl1 = cl + (cl*0.05)
-            x = round(cl1,0)
-            y = round(cl,0)
-            vmo = (b*c*0.13)-(y*0.09*0.13*0.24)
-            print("La cantidad de ladrillos es",x,"considerando el 5% de desperdicio")
-            print("La cantidad de bolsas de cemento es", vmo*7.5)
-            print("La cantidad de arena guesa es",vmo*1.05)
-        elif e =="Cabeza":
-            cl = (b*c)/((0.13 + 0.015)*(0.09+ 0.015))
-            cl1 = cl + (cl1*0.05)
-            x = round(cl1,0)
-            print("La cantidad de ladrillos es",x,"considerando el 5% de desperdicio")
+        cl = (b*c)/((0.24 + 0.015)*(0.09+ 0.015))
+        cl1 = cl + (cl*0.05)
+        x = round(cl1,0)
+        y = round(cl,0)
+        vmo = (b*c*0.13)-(y*0.09*0.13*0.24)
+        cemento = (vmo * 7.5)/(b*c)
+        arena_gruesa = (vmo*1.05)/(b*c)
+        cem = round(cemento,2)
+        are = round(arena_gruesa,2)
+        print("La cantidad de ladrillos es",x,"considerando el 5% de desperdicio")
+        print("La cantidad de bolsas de cemento es",cem, "m3")
+        print("La cantidad de arena guesa es",are,"m3")
+        
     elif d == "Pandereta":
         e = input("El tipo de aparejo sera:(Soga/Cabeza)")
-        if e == "Soga":
-            cl = (b*c)/((0.24 + 0.015)*(0.09+ 0.015))
-            cl1 = cl + (cl*0.05)
-            x = round(cl1,0)
-            print("La cantidad de ladrillos es",x,"considerando el 5% de desperdicio")
-        elif e =="Cabeza":
-            cl = (b*c)/((0.12 + 0.015)*(0.09+ 0.015))
-            cl1 = cl + (cl*0.05)
-            x = round(cl1,0)
-            print("La cantidad de ladrillos es",x,"considerando el 5% de desperdicio")
-
-
-x= float(input("Registrar ancho del piso en cm"))
-y= float(input("Registrar largo del piso en cm"))
-lo= float(input("Registre ancho de loza en cm"))
-sa= float(input("Registre largo de loza en cm"))
-#Numero de piezas
-t= (x*y)
-l=(lo*sa)
-u=t/l
-w= round(u,0)
-print("Losas requeridas", w)
-#Cuantas losas adicional/10% de desperdicio
-g= w*0.01 
-h= round(g,0)
-print("Losas adicionales", h)
-#Losas en total
-r= u+h
-p= round(r,0)
-print("En total necesitaremos",p, "losas")
-
-t = input("ingrese el tipo de construccion:")
-
-if t == "Techo de losa":
+        cl = (b*c)/((0.24 + 0.015)*(0.09+ 0.015))
+        cl1 = cl + (cl*0.05)
+        x = round(cl1,0)
+        y = round(cl,0)
+        vmo = (b*c*0.13)-(y*0.09*0.13*0.24)
+        cemento = (vmo * 7.5)/(b*c)
+        arena_gruesa = (vmo*1.05)/(b*c)
+        cem = round(cemento,2)
+        are = round(arena_gruesa,2)
+        print("La cantidad de ladrillos es",x,"considerando el 5% de desperdicio")
+        print("La cantidad de bolsas de cemento es",cem, "m3")
+        print("La cantidad de arena guesa es",are,"m3")
+    
+elif a == "Techo de losa":
     ancho = float(input("Ingrese el ancho:"))   # ancho de techo
     largo = float(input("Ingrese el largo:"))   #longitud de techo
     espesor = 0.25                              # espesor fijo de techo
